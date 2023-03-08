71640b0068422aefdbb294aa7b0d443f440f3116
KK-Romanov <122021943+KK-Romanov@users.noreply.github.com>
2023-03-08 11:48:01 +0000

    [Finish] 4-27

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+    def index
+        
+    end
+    def create
+        
+    end
+    def show
+        
+    end
+    def update
+        
+    end
+    def edit
+        
+    end
+    
+    def destroy
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    
+    def edit 
+        
+    end 
+    
+    def index
+        
+    end 
+    
+    def show
+        
+    end
+    
+    def update
+        
+    end
+end
