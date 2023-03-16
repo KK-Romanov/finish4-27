09a6a465f9e0d0556f08541c1bba58aa964425ce
KK-Romanov <122021943+KK-Romanov@users.noreply.github.com>
2023-03-16 13:16:46 +0000

    3

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,13 +3,13 @@ class Book < ApplicationRecord
     
     belongs_to :user
     
-    def get_image
-    if image.attached?
-      image
-    else
-      'no_image.jpg'
-    end
-    end
+    # def get_image
+    # if image.attached?
+    #   image
+    # else
+    #   'no_image.jpg'
+    # end
+    # end
 
     
 end
